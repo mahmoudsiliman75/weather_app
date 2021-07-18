85d01544fb5692e8b869afd3ca3d26cd9a45efa6
mahmoudsiliman75 <mahmoudsiliman75@gmail.com>
2021-07-18 05:30:58 -0700

    finish searchbox styles

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,22 @@
 <template>
   <div id="app">
-    <h1> Hi From Weather App </h1>
+    <main>
+      <div class="search_box">
+        <input type="text" placeholder="Search...." id="search_input">
+      </div>
+
+      <div class="weather_wraper">
+        <div class="location_box">
+          <div class="location"> Cairo, EGY </div>
+          <div class="date"> Monday 20 January 2021 </div>
+        </div>
+
+        <div class="weather_box">
+          <div class="temp"> 17°c </div>
+          <div class="weather"> Rain </div>
+        </div>
+      </div>
+    </main>
   </div>
 </template>
 
--- src/store/index.js
+++ src/store/index.js
@@ -4,8 +4,19 @@ import Vuex from "vuex";
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
+  // START:: VUEX STATE
+  state() {
+    return{
+      api_key: "44393ac3aced44e3d40e9c5a3100987a",
+    }
+  },
+  // END:: VUEX STATE
+
+  // START:: VUEX MUTATIONS
   mutations: {},
+  // END:: VUEX MUTATIONS
+
+  // START:: VUEX ACTIONS
   actions: {},
-  modules: {},
+  // END:: VUEX ACTIONS
 });
